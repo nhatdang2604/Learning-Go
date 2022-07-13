b56d280d24adff4ed4edf48ee76782aba9a36b9c
nhatdang2604 <nhatdang2604@gmail.com>
2022-07-13 15:55:21 +0700

    Finish chap 11

--- tutorial.go
+++ tutorial.go
@@ -5,13 +5,13 @@ import (
 )
 
 func main() {
-	x := 0
-	for x <= 5 {
-		fmt.Println(x)
-		x++
-	}
-
-	for x := 0; x <= 5; x++ {
-		fmt.Println(x)
+	ans := -1
+	switch {
+	case ans > 0:
+		fmt.Println("Greater than 0")
+	case ans < 0:
+		fmt.Println("Less than 0")
+	default:
+		fmt.Println("zero")
 	}
 }
