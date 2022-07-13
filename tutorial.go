8514106589b32c0b6876e345bb11f268f243ed8a
nhatdang2604 <nhatdang2604@gmail.com>
2022-07-13 14:12:06 +0700

    init for chap 6

--- tutorial.go
+++ tutorial.go
@@ -1,22 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	// fmt.Printf("Type something: ")
-	// scanner.Scan()
-	// input := scanner.Text()
-	// fmt.Printf("You typed: %q", input)
-
-	fmt.Printf("Type an integer number: ")
-	scanner.Scan()
-	input, _ := strconv.ParseInt(scanner.Text(), 10, 32)
-	fmt.Printf("You typed: %d", input)
-
+	var num1 int = 8
+	var num2 int = 4
+	answer := 0
+	fmt.Printf("%d", answer)
 }
