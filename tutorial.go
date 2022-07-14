7b8d50016fd4b9b5e2ca931ea41a52239c915a39
nhatdang2604 <nhatdang2604@gmail.com>
2022-07-14 13:36:55 +0700

    Finish chap 13

--- tutorial.go
+++ tutorial.go
@@ -7,6 +7,12 @@ import (
 func main() {
 	var x []int = []int{1, 2, 3, 4, 5}
 	var s []int = x[1:3]
+	b := append(s, 10)
+	b[0] = 123
 	fmt.Print(x[:cap(x)])
 	fmt.Println(s[:cap(s)])
+	fmt.Println(b)
+
+	a := make([]int, 5)
+	fmt.Println(a)
 }
