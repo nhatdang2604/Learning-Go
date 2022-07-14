8e3fea1ec604b7fb00e7737104eb22bc64092f09
nhatdang2604 <nhatdang2604@gmail.com>
2022-07-14 13:31:55 +0700

    testing for slice

--- tutorial.go
+++ tutorial.go
@@ -5,18 +5,8 @@ import (
 )
 
 func main() {
-	var arr []int = []int{4, 5, 6}
-	var arr2d [][]int = [][]int{{1, 2}, {3, 4}}
-
-	fmt.Println(arr)
-	fmt.Println(arr2d)
-
-	sum := []int{0, 0}
-	var size int = len(arr2d)
-	for i := 0; i < size; i++ {
-		sum[0] += arr2d[i][0]
-		sum[1] += arr2d[i][1]
-	}
-
-	fmt.Println(sum)
+	var x []int = []int{1, 2, 3, 4, 5}
+	var s []int = x[1:3]
+	fmt.Print(x[:cap(x)])
+	fmt.Println(s[:cap(s)])
 }
