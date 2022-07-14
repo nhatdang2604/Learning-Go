31ecd7385b0e048f428543f53509f265663210c9
nhatdang2604 <nhatdang2604@gmail.com>
2022-07-14 15:34:27 +0700

    Finish chap 16

--- tutorial.go
+++ tutorial.go
@@ -4,22 +4,23 @@ import (
 	"fmt"
 )
 
+func testBody() int {
+	fmt.Println("Inside testBody()")
+	return -2
+}
+
+func test() int {
+	defer fmt.Println("test")
+	return testBody()
+}
+
+func addAndMinus(x, y int) (add, minus int) {
+	add = x + y
+	minus = x - y
+	return
+}
+
 func main() {
-	var mp map[string]int = map[string]int{
-		"apple":  5,
-		"pear":   6,
-		"orange": 9,
-	}
-
-	delete(mp, "apple")
-	mp["pear"] = 1234
-	fmt.Println(mp)
-	fmt.Println(mp["apple"])
-
-	val, err := mp["apocalypse"]
-	fmt.Println(val, err)
-
-	for key, value := range mp {
-		fmt.Println(key, value)
-	}
+	fmt.Println(addAndMinus(12, 13))
+	fmt.Println(test())
 }
