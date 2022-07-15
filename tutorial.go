d02fea41dbc8d28a6ade01e7be5027b8f4d1e3db
nhatdang2604 <nhatdang2604@gmail.com>
2022-07-15 15:49:58 +0700

    Finish chap 22

--- tutorial.go
+++ tutorial.go
@@ -1,8 +1,12 @@
 package main
 
-import {
+import (
 	"fmt"
 	"math"
+)
+
+type Shape interface {
+	area() float32
 }
 
 type Circle struct {
@@ -10,7 +14,7 @@ type Circle struct {
 }
 
 type Rectangle struct {
-	width float32
+	width  float32
 	height float32
 }
 
@@ -18,11 +22,29 @@ func (rect Rectangle) area() float32 {
 	return rect.width * rect.height
 }
 
-func (circ Circle) area() float32 {
-	return math.Pi * circ.radius * circ.radius;
+func (rect Rectangle) getHeight() float32 {
+	return rect.height
 }
 
+func (circ Circle) area() float32 {
+	return math.Pi * circ.radius * circ.radius
+}
 
 func main() {
-	
+	circ := Circle{4.5}
+	rect := Rectangle{5, 7}
+
+	shapes := []Shape{circ, rect}
+
+	fmt.Println(circ)
+	fmt.Println(rect)
+	fmt.Println(shapes)
+
+	for _, shape := range shapes {
+		fmt.Println(shape.area())
+	}
+
+	//Type conversion
+	var test Rectangle = shapes[1].(Rectangle)
+	fmt.Println(test.getHeight())
 }
