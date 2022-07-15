08544d498654ec3fceacf9dd41cf0373468cde41
nhatdang2604 <nhatdang2604@gmail.com>
2022-07-15 15:25:58 +0700

    Init for chap 22

--- tutorial.go
+++ tutorial.go
@@ -1,56 +1,28 @@
 package main
 
-import "fmt"
-
-type Student struct {
-	name   string
-	grades []int
-	age    int
+import {
+	"fmt"
+	"math"
 }
 
-func (student Student) getAge() int {
-	return student.age
+type Circle struct {
+	radius float32
 }
 
-//Must use pointer to make the 'this' student pass by reference
-func (student *Student) setAge(age int) {
-	student.age = age
+type Rectangle struct {
+	width float32
+	height float32
 }
 
-func (student Student) getAVGGrade() float32 {
-	result := 0
-	for _, grade := range student.grades {
-		result += grade
-	}
-
-	return float32(result) / float32(len(student.grades))
+func (rect Rectangle) area() float32 {
+	return rect.width * rect.height
 }
 
-func (student Student) getMaxGrade() (index, value int) {
-	index = -1
-	value = -1
-	if 0 == len(student.grades) {
-		return
-	}
-
-	index = 0
-	value = student.grades[index]
-
-	for i, v := range student.grades {
-		if value < v {
-			value = v
-			index = i
-		}
-	}
-
-	return
+func (circ Circle) area() float32 {
+	return math.Pi * circ.radius * circ.radius;
 }
+
 
 func main() {
-	var Tuấn = Student{"Tuấn", []int{1, 2, 3, 5}, 17}
-	fmt.Println(Tuấn, Tuấn.getAge())
-	Tuấn.age = 18
-	Tuấn.setAge(123)
-	fmt.Println(Tuấn, Tuấn.getAge(), Tuấn.getAVGGrade())
-	fmt.Println(Tuấn.getMaxGrade())
+	
 }
