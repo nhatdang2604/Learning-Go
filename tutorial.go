84ce27bb2b2e6958b02406e9b57239a2ba12fbe5
nhatdang2604 <nhatdang2604@gmail.com>
2022-07-15 15:20:35 +0700

    Finish chap 21

--- tutorial.go
+++ tutorial.go
@@ -2,31 +2,55 @@ package main
 
 import "fmt"
 
-type Point struct {
-	x int32
-	y int32
+type Student struct {
+	name   string
+	grades []int
+	age    int
 }
 
-type Circle struct {
-	center *Point
-	radius float32
+func (student Student) getAge() int {
+	return student.age
 }
 
-//Spreading like js
-type SpreadingCircle struct {
-	*Point
-	radius float32
+//Must use pointer to make the 'this' student pass by reference
+func (student *Student) setAge(age int) {
+	student.age = age
 }
 
-func changePoint(point Point) {
-	point.x = 123
-	point.y = 456
+func (student Student) getAVGGrade() float32 {
+	result := 0
+	for _, grade := range student.grades {
+		result += grade
+	}
+
+	return float32(result) / float32(len(student.grades))
+}
+
+func (student Student) getMaxGrade() (index, value int) {
+	index = -1
+	value = -1
+	if 0 == len(student.grades) {
+		return
+	}
+
+	index = 0
+	value = student.grades[index]
+
+	for i, v := range student.grades {
+		if value < v {
+			value = v
+			index = i
+		}
+	}
+
+	return
 }
 
 func main() {
-	var center Point = Point{y: 1, x: 2}
-	var circle Circle = Circle{radius: 2, center: &center}
-	var spread_circle SpreadingCircle = SpreadingCircle{&Point{1, 2}, 4}
-	changePoint(center)
-	fmt.Println(circle, center, spread_circle.x, spread_circle.y, spread_circle.radius)
+	var Tuấn = Student{"Tuấn", []int{1, 2, 3, 5}, 17}
+	fmt.Println(Tuấn, Tuấn.getAge())
+	Tuấn.age = 18
+	Tuấn.setAge(123)
+	fmt.Println(Tuấn, Tuấn.getAge(), Tuấn.getAVGGrade())
+	fmt.Println(Tuấn.getMaxGrade())
 }
