e5a5fe99ad6fa0c16ecb5aaa0bff73a56c0238f1
nhatdang2604 <nhatdang2604@gmail.com>
2022-07-15 22:52:06 +0700

    Finish Sclices exercise

---
+++ slices.go
@@ -0,0 +1,22 @@
+package main
+
+
+import (
+	"golang.org/x/tour/pic"
+)
+
+func Pic(dx, dy int) [][]uint8 {
+	var image = make([][]uint8, dy)
+	for i := range image {
+		image[i] = make([]uint8, dx)
+		for j := range image[i] {
+			image[i][j] = uint8((i+j)/2);
+		}
+	}
+	
+	return image
+}
+
+func main() {
+	pic.Show(Pic)
+}
