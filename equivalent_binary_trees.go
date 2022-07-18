10d6e14665d881465af7290f453b4fc44d1c2d1a
nhatdang2604 <nhatdang2604@gmail.com>
2022-07-18 15:08:03 +0700

    Change the order of Walk function to LNR

--- equivalent_binary_trees.go
+++ equivalent_binary_trees.go
@@ -1,23 +1,12 @@
 package main
 
-import "golang.org/x/tour/tree"
-
-// Walk walks the tree t sending all values
-// from the tree to the channel ch.
-func Walk(t *tree.Tree, ch chan int) {
-	if nil == t {
-		return
-	}
-
-	Walk(t.Left)
-	Walk(t.Right)
-
-	ch <- t.Value
-}
-
-// Same determines whether the trees
-// t1 and t2 contain the same values.
-func Same(t1, t2 *tree.Tree) bool
+import (
+	"fmt"
+)
 
 func main() {
+	ch := make(chan int)
+	ch <- 1
+
+	fmt.Println(<-ch)
 }
