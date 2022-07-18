1528d1637e1525229d82d082aa8d9669966678b3
nhatdang2604 <nhatdang2604@gmail.com>
2022-07-18 17:42:31 +0700

    Finish Web Crawler Problem

--- web_crawler.go
+++ web_crawler.go
@@ -18,6 +18,7 @@ func Crawl(url string, depth int, fetcher Fetcher) {
 	// TODO: Don't fetch the same URL twice.
 	// This implementation doesn't do either:
 
+	visitedMap.visit(url)
 	defer waitor.Done()
 
 	if depth <= 0 {
@@ -33,8 +34,10 @@ func Crawl(url string, depth int, fetcher Fetcher) {
 	fmt.Printf("found: %s %q\n", url, body)
 
 	for _, u := range urls {
-		waitor.Add(1)
-		go Crawl(u, depth-1, fetcher)
+		if !visitedMap.isVisited(u) {
+			waitor.Add(1)
+			go Crawl(u, depth-1, fetcher)
+		}
 	}
 
 	return
@@ -84,38 +87,41 @@ func (f fakeFetcher) Fetch(url string) (string, []string, error) {
 	return "", nil, fmt.Errorf("not found: %s", url)
 }
 
-var waitor = sync.WaitGroup{}
-
 // fetcher is a populated fakeFetcher.
-var fetcher = fakeFetcher{
-	"https://golang.org/": &fakeResult{
-		"The Go Programming Language",
-		[]string{
-			"https://golang.org/pkg/",
-			"https://golang.org/cmd/",
+var (
+	fetcher = fakeFetcher{
+		"https://golang.org/": &fakeResult{
+			"The Go Programming Language",
+			[]string{
+				"https://golang.org/pkg/",
+				"https://golang.org/cmd/",
+			},
 		},
-	},
-	"https://golang.org/pkg/": &fakeResult{
-		"Packages",
-		[]string{
-			"https://golang.org/",
-			"https://golang.org/cmd/",
-			"https://golang.org/pkg/fmt/",
-			"https://golang.org/pkg/os/",
+		"https://golang.org/pkg/": &fakeResult{
+			"Packages",
+			[]string{
+				"https://golang.org/",
+				"https://golang.org/cmd/",
+				"https://golang.org/pkg/fmt/",
+				"https://golang.org/pkg/os/",
+			},
 		},
-	},
-	"https://golang.org/pkg/fmt/": &fakeResult{
-		"Package fmt",
-		[]string{
-			"https://golang.org/",
-			"https://golang.org/pkg/",
+		"https://golang.org/pkg/fmt/": &fakeResult{
+			"Package fmt",
+			[]string{
+				"https://golang.org/",
+				"https://golang.org/pkg/",
+			},
 		},
-	},
-	"https://golang.org/pkg/os/": &fakeResult{
-		"Package os",
-		[]string{
-			"https://golang.org/",
-			"https://golang.org/pkg/",
+		"https://golang.org/pkg/os/": &fakeResult{
+			"Package os",
+			[]string{
+				"https://golang.org/",
+				"https://golang.org/pkg/",
+			},
 		},
-	},
-}
+	}
+
+	waitor     = sync.WaitGroup{}
+	visitedMap = VisitedMap{value: make(map[string]bool)}
+)
