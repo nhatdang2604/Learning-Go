453481b5d1c9332072bdbbc64ff82c137dcee9b5
nhatdang2604 <nhatdang2604@gmail.com>
2022-07-18 15:22:19 +0700

    Add Same()

--- equivalent_binary_trees.go
+++ equivalent_binary_trees.go
@@ -21,7 +21,33 @@ func WalkRecursive(t *tree.Tree, ch chan int) {
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
-func Same(t1, t2 *tree.Tree) bool
+func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+	var buffer1, buffer2 [10]int
+
+	i := 0
+	for v := range ch1 {
+		buffer1[i] = v
+		i++
+	}
+
+	i = 0
+	for v := range ch2 {
+		buffer2[i] = v
+		i++
+	}
+
+	for i, v := range buffer1 {
+		if v != buffer2[i] {
+			return false
+		}
+	}
+
+	return true
+}
 
 func main() {
 }
