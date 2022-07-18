7db4576121fd49a684218d154d28e0e65da2d562
nhatdang2604 <nhatdang2604@gmail.com>
2022-07-18 15:21:34 +0700

    Split Walk() to Walk() and WalkRecursive()

--- equivalent_binary_trees.go
+++ equivalent_binary_trees.go
@@ -1,12 +1,27 @@
 package main
 
-import (
-	"fmt"
-)
+import "golang.org/x/tour/tree"
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	WalkRecursive(t, ch)
+	close(ch)
+}
+
+func WalkRecursive(t *tree.Tree, ch chan int) {
+	if nil == t {
+		return
+	}
+
+	WalkRecursive(t.Left, ch)
+	ch <- t.Value
+	WalkRecursive(t.Right, ch)
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool
 
 func main() {
-	ch := make(chan int)
-	ch <- 1
-
-	fmt.Println(<-ch)
 }
